3a361eb0758cb7226926688ae4d74f1cf5931be2
kassing3 <108312371+kassing3@users.noreply.github.com>
2022-12-20 11:41:07 -0500

    Routes

--- models/index.js
+++ models/index.js
@@ -5,15 +5,20 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category);
+Product.belongsTo(Category, {
+  foreignKey: "category_id"
+});
 
 // Categories have many Products
-Category.hasMany(Product);
+Category.hasMany(Product, {
+  foreignKey: "category_id"
+});
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: {
     model: ProductTag,
+    foreignKey: 'product_id',
     unique: false
   },
   as: "Products_Tagged"
@@ -23,6 +28,7 @@ Product.belongsToMany(Tag, {
 Tag.belongsToMany(Product, {
   through: {
     model: ProductTag,
+    foreignKey: 'tag_id',
     unique: false
   },
   as: "Tagged_Products"
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,58 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+ try{
+  const productData =  await Product.findAll({
+    include: [{
+      model: Category
+    },
+    {
+      model: Tag,
+      through: ProductTag,
+      as: "Tagged_Products"
+    }
+  ]
+  });
+
+  res.status(200).json(productData);
+
+ } catch (err) {
+  res.status(500).json(err);
+}
+
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+
+  try {
+    const productData =  await Product.findByPk(req.params.id, {
+      include: [{
+        model: Tag,
+        through: ProductTag,
+        as: "Products_Tagged"
+      },
+      {
+        model: Category
+      }
+    ]
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
 // create new product
